0e6acda2e71d62540b81ea794225ea9fd7d6ff0c
alberto romano <aromanog1@gmail.com>
2024-04-07 18:57:04 -0600

    :)

--- test_mcd.py
+++ test_mcd.py
@@ -5,19 +5,25 @@ from mcd import mcd
 class TestStringMethods(unittest.TestCase):
     ''' class to test mcd.py '''
     def test_case_1(self):
-        ''' Negative input '''
-        self.assertEqual(mcd(-1, 10), -1)
+        ''' X = 0 input '''
+        self.assertEqual(mcd(0, 10), -1)
     def test_case_2(self):
-        ''' input as 1 '''
-        self.assertEqual(mcd(1, 10), 1)
+        ''' y = 0 input '''
+        self.assertEqual(mcd(10, 0), -1)
     def test_case_3(self):
-        ''' Same input '''
-        self.assertEqual(mcd(10, 10), 10)
+        ''' x = 1 input '''
+        self.assertEqual(mcd(1, 10), 1)
     def test_case_4(self):
-        ''' x > y '''
-        self.assertEqual(mcd(10, 5), 5)
+        ''' y = 1 input '''
+        self.assertEqual(mcd(10, 1), 1)
     def test_case_5(self):
-        ''' y > x '''
+        ''' x = y input '''
+        self.assertEqual(mcd(10, 10), 10)
+    def test_case_6(self):
+        ''' x > y input '''
+        self.assertEqual(mcd(10, 5), 5)
+    def test_case_7(self):
+        ''' y > x input '''
         self.assertEqual(mcd(5, 10), 5)
 
 if __name__ == '__main__':
